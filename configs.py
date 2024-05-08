1c879f5f71e8df36f5a88f8c8bcbea8339a05dc4
Xobruce <130820625+BruceMorpho@users.noreply.github.com>
2024-05-08 15:06:12 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -6,11 +6,11 @@ class Config:
     API_ID = int(getenv("API_ID", "20239261"))
     API_HASH = getenv("API_HASH", "af61dab399ed3220a3a81570b56bd29d")
     BOT_TOKEN = getenv("BOT_TOKEN", "6694252129:AAGobWHGc6XbaZwfXgAf-5NJ_u3L9SRSPvo")
-    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "DP_BOTZ")
-    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1002008853384"))
+    UPDATE_CHANNEL = getenv("UPDATE_CHANNEL", "XozMovies")
+    UPDATECHANNEL_ID = int(getenv("UPDATECHANNEL_ID", "-1002139227512"))
     ADMIN = list(map(int, getenv("ADMIN", "1242556540 1058787811").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://evamusicbot:mrwaris04@cluster0.5ad8zuj.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001789244683"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002132087730"))
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
